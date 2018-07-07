ef6880da5cf4c4588f3e33c73d986354f5fa13ae
Juan Morales <crispamares@gmail.com>
2018-07-07 15:24:27 +0200

    first attempt to implement clap in scales

--- src/scale.rs
+++ src/scale.rs
@@ -1,23 +1,41 @@
-use std::ops::{Sub, Mul, Div};
+use std::ops::{Sub, Mul, Div, };
 use std::iter::{FromIterator};
 use std::convert::{From};
+
 
 #[derive(Debug, Builder, Default, PartialEq)]
 pub struct Scale<D, R> {
     pub domain: Vec<D>,
-    pub range: Vec<R>
+    pub range: Vec<R>,
+    #[builder(default)]
+    pub clap: bool
 }
 
-impl<D, R> Scale<D, R> where 
+impl<D, R> Scale<D, R> where
     D: From<R> + Sub<Output=D> + Mul<Output=D> + Div<Output=D> + Copy,
-    R: Sub<Output=D> + Copy,
+    R: From<D> + Sub<Output=R> + Mul<Output=R> + Div<Output=R> + Copy,
+    Vec<D>: FromIterator<R>,
     Vec<R>: FromIterator<D>
 {
     pub fn call(&self, data: &[D]) -> Vec<R> {
         data.iter()
-            .map(|&x| x 
-                * (self.range[1] - self.range[0])
-                / (self.domain[1] - self.domain[0]) )
+            .map(|&x| {
+                let val = x 
+                    * (self.range[1] - self.range[0]).into()
+                    / (self.domain[1] - self.domain[0]); 
+                if self.clap { self.range[1].min(val.into()).max(self.range[0]) } else { val.into() }
+            })
+            .collect()
+    }
+
+    pub fn invert(&self, data: &[R]) -> Vec<D> {
+        data.iter()
+            .map(|&x| { 
+                let val = x 
+                    * (self.domain[1] - self.domain[0]).into()
+                    / (self.range[1] - self.range[0]);
+                if self.clap { self.domain[1].min(val.into()).max(self.domain[0]) } else { val.into() }
+                })
             .collect()
     }
 }
@@ -31,9 +49,9 @@ mod tests {
         let scale: Scale<f32, f32> = ScaleBuilder::default()
             .domain(vec![1.0, 2.0])
             .range(vec![10.0, 20.0])
+            .clap(false)
             .build().unwrap();
-
-        assert_eq!(scale, Scale{range: vec![10.0, 20.0], domain: vec![1.0, 2.0]});
+        assert_eq!(scale, Scale{range: vec![10.0, 20.0], domain: vec![1.0, 2.0], clap: false});
     }
 
     #[test]
@@ -43,6 +61,16 @@ mod tests {
             .range(vec![10.0, 20.0])
             .build().unwrap();
 
-        assert_eq!( scale.call(&[1.0, 2.0, 3.0, 4.0, 5.0]), vec![10.0, 20.0, 30.0, 40.0, 50.0] );
+        assert_eq!( scale.call(&[1.0, 2.0, 3.0, 4.0, 5.0]), [10.0, 20.0, 30.0, 40.0, 50.0] );
+    }
+
+    #[test]
+    fn invert_works() {
+        let scale: Scale<f32, f32> = ScaleBuilder::default()
+            .domain(vec![1.0, 2.0])
+            .range(vec![10.0, 20.0])
+            .build().unwrap();
+
+        assert_eq!( scale.invert(&[10.0, 20.0, 30.0, 40.0, 50.0]), [1.0, 2.0, 3.0, 4.0, 5.0]);
     }
 }
