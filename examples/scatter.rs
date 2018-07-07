9317e3ff1c3297501d9523670a8d4c9849ef4717
Juan Morales <crispamares@gmail.com>
2018-07-07 20:15:20 +0200

    Add scatter example

--- examples/scatter.rs
+++ examples/scatter.rs
@@ -1,13 +1,39 @@
 extern crate helio;
+extern crate csv;
 
 use std::rc::Rc;
 use std::error::Error;
 
-use helio::core::{Color, Canvas, Circle, CircleBuilder, StyleBuilder, Scene, Rect};
-use helio::scale::{Scale};
+use helio::core::{Color, Canvas, Circle, CircleBuilder, StyleBuilder, Scene};
+use helio::scale::{Scale, ScaleBuilder, extend};
 use helio::svg_backend;
 
 fn main() -> Result<(), Box<Error>> {
+
+    let width = 800;
+    let height = 600;
+
+    let x_column = "weight (lb)";
+    let y_column = "displacement (cc)";
+
+    // Data to use
+    let mut x_data: Vec<f32> = Vec::new();
+    let mut y_data: Vec<f32> = Vec::new();
+
+    let mut rdr = csv::Reader::from_path("examples/data/cars.csv")?;
+    println!("{:?}", rdr.headers()?);
+
+    let x_data_idx = rdr.headers()?.iter().position(|x| x == x_column)
+        .ok_or(format!("'{}' not in the dataset", x_column) )?;
+    let y_data_idx = rdr.headers()?.iter().position(|x| x == y_column)
+        .ok_or(format!("'{}' not in the dataset", y_column) )?;
+
+    for result in rdr.records() {
+        let record = result?;
+        x_data.push(record.get(x_data_idx).unwrap().parse()?);
+        y_data.push(record.get(y_data_idx).unwrap().parse()?);
+    }
+
     let white = Color{r:0, g:0, b:0, a:1.0};
     let blue = Color{r:0, g:0, b:255, a:0.5};
     let style = Rc::new(
@@ -16,31 +42,37 @@ fn main() -> Result<(), Box<Error>> {
             .build()?
     );
     let canvas = Canvas {
-        width: 800,
-        height: 600,
+        width: width,
+        height: height,
         background: white
     };
 
-    let data = [{}];
-
-    let circle : Circle = CircleBuilder::default()
-        .x(canvas.width / 2)
-        .y(canvas.height / 2)
-        .radius(40)
-        .style(style.clone())
+    let x_scale: Scale<f32, f32> = ScaleBuilder::default()
+        .domain(extend(&x_data))
+        .range([0.0, width as f32])
         .build()?;
 
-    let rect = Rect {
-        x: canvas.width / 4,
-        y: canvas.height / 4,
-        width: 40,
-        height: 40,
-        style: style.clone()
-    };
+    let y_scale: Scale<f32, f32> = ScaleBuilder::default()
+        .domain(extend(&y_data))
+        .range([height as f32, 0.0])
+        .build()?;
 
+    print!("{:?} · ", &x_scale);
     let mut scene = Scene::new(canvas);
-    scene.add(Box::new(circle));
-    scene.add(Box::new(rect));
+    for r in x_scale.call(&x_data).iter().zip(y_scale.call(&y_data).iter()) {
+
+        let (x, y) = r;
+        print!("{:?} · ", &x);
+        let circle : Circle = CircleBuilder::default()
+            .x(*x as u32)
+            .y(*y as u32)
+            .radius(10)
+            .style(style.clone())
+            .build()?;
+
+        scene.add(Box::new(circle));
+        
+    }
 
     svg_backend::save("chart.svg", &scene);
     Ok(())
---
+++ examples/simple.rs
@@ -0,0 +1,47 @@
+extern crate helio;
+
+use std::rc::Rc;
+use std::error::Error;
+
+use helio::core::{Color, Canvas, Circle, CircleBuilder, StyleBuilder, Scene, Rect};
+use helio::scale::{Scale};
+use helio::svg_backend;
+
+fn main() -> Result<(), Box<Error>> {
+    let white = Color{r:0, g:0, b:0, a:1.0};
+    let blue = Color{r:0, g:0, b:255, a:0.5};
+    let style = Rc::new(
+        StyleBuilder::default()
+            .fill(Some(blue))
+            .build()?
+    );
+    let canvas = Canvas {
+        width: 800,
+        height: 600,
+        background: white
+    };
+
+    let data = [{}];
+
+    let circle : Circle = CircleBuilder::default()
+        .x(canvas.width / 2)
+        .y(canvas.height / 2)
+        .radius(40)
+        .style(style.clone())
+        .build()?;
+
+    let rect = Rect {
+        x: canvas.width / 4,
+        y: canvas.height / 4,
+        width: 40,
+        height: 40,
+        style: style.clone()
+    };
+
+    let mut scene = Scene::new(canvas);
+    scene.add(Box::new(circle));
+    scene.add(Box::new(rect));
+
+    svg_backend::save("chart.svg", &scene);
+    Ok(())
+}
